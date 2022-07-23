77e7359e927d95c36b07d6fdfc68e55d1474bafc
Surabhiverma <surabhiverma4@gmail.com>
2022-07-24 00:00:36 +0100

    Initial Commit

---
+++ CrossD Proto V3/CrossD_Proto_V3App.swift
@@ -0,0 +1,17 @@
+//
+//  CrossD_Proto_V3App.swift
+//  CrossD Proto V3
+//
+//  Created by Surabhi Verma on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CrossD_Proto_V3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
